23d4492f9275bad4d1d26531347aa28522859601
Nick Meehan <nnmeehan@gmail.com>
2014-05-04 15:14:50 -0700

    Added error handling for whether text message actually goes through or not. Still need to create contacts to test these processes.

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -5,14 +5,13 @@ end
 
 post '/messages' do
 	content = Message.convert_to_yoda(params[:content])
+	# contact_id = params[:contact_id]
+	string = params[:content]
 
 	# This method will need to be changed once contacts are instituted
+	# and it will become an instance method for the current user.
+	# User.find(current_user).send_message(content, contact_id)
 	User.send_message(content)
-
-	
-
-  p message.sid
-
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,28 @@ class User < ActiveRecord::Base
 
 
 
-  def self.send_message(content)
+  def self.send_message(content, contact_id = 0)
 		account_sid = ENV['ACCOUNT_SID']
 		auth_token = ENV['AUTH_TOKEN']
+		begin
+			@client = Twilio::REST::Client.new account_sid, auth_token
+			# The :to and :from in this case will change depending on the User and the Contact
+			# :to => self.phone_number
+			# :from => Contact.find(contact_id).phone_number
+			message = @client.account.messages.create(
+				:body => "#{content}",
+			  :to => ENV['MY_NUMBER'],
+			  :from => ENV['TWILIO_NUMBER']
+		  )
+		rescue Twilio::REST::RequestError => e
+			flash[:notice] = e.message
+		end
 
-		@client = Twilio::REST::Client.new account_sid, auth_token
-	 
-		message = @client.account.messages.create(
-		:body => "#{content}",
-	  :to => ENV['MY_NUMBER'],
-	  :from => ENV['TWILIO_NUMBER'])
+		if flash[:notice]
+			return flash[:notice]
+		else
+			self.messages.create(content: message.body, contact_id: contact_id, message_sid: message.sid)
+			return "success"
+		end
   end
 end
