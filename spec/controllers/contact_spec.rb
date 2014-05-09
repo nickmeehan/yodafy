4c7675a03dcd5ad3a8a5ba8133000b86df2eb6f0
Nick Meehan <nnmeehan@gmail.com>
2014-05-08 18:06:40 -0700

    get /contacts/:id tests passing.

--- spec/controllers/contact_spec.rb
+++ spec/controllers/contact_spec.rb
@@ -6,7 +6,7 @@ describe "get /contacts/new route" do
 		## Arrange
 
 		## Act
-		get '/contacts/new'
+		get "/contacts/new"
 
 		## Assert
 		expect(last_response.body).to include("add_contact_form")
@@ -19,14 +19,14 @@ describe "post /contacts route" do
 	it "saves a new contact to the database" do
 		## Arrange
 		User.destroy_all
-		@user = User.create(phone_number: "+14033130220",
+		@user = User.create(phone_number: "+14033030220",
 												email: "nick@nick.ca",
 												password: "password")
 		fake_session = { 'rack.session' => { user_id: @user.id } }
 		params = { phone_number: "+14039991234", name: "Han Solo" }
 
 		## Act
-		post '/contacts', params, fake_session
+		post "/contacts", params, fake_session
 
 		## Assert
 		expect(User.find(@user.id).contacts.count).to eq(1)
@@ -36,14 +36,14 @@ describe "post /contacts route" do
 	it "sends a success message in json if contact is saved" do
 		## Arrange
 		User.destroy_all
-		@user = User.create(phone_number: "+14033130220",
+		@user = User.create(phone_number: "+14033030220",
 												email: "nick@nick.ca",
 												password: "password")
 		fake_session = { 'rack.session' => { user_id: @user.id } }
 		params = { phone_number: "+14039991234", name: "Han Solo" }
 
 		## Act
-		post '/contacts', params, fake_session
+		post "/contacts", params, fake_session
 
 		## Assert
 		expected_output = { success: "A new padawan, you have." }.to_json
@@ -54,14 +54,14 @@ describe "post /contacts route" do
 	it "sends an error message as json string" do
 		## Arrange
 		User.destroy_all
-		@user = User.create(phone_number: "+14033130220",
+		@user = User.create(phone_number: "+14033030220",
 												email: "nick@nick.ca",
 												password: "password")
 		fake_session = { 'rack.session' => { user_id: @user.id } }
 		params = { phone_number: "+14039991234" }
 
 		## Act
-		post '/contacts', params, fake_session
+		post "/contacts", params, fake_session
 
 		## Assert
 		expected_output = { errors: ["Name can't be blank"] }.to_json
@@ -70,3 +70,73 @@ describe "post /contacts route" do
 	end
 
 end
+
+describe "get /contacts/:id route" do
+
+	it "sends back the correct contact information as a json string" do
+		## Arrange
+		User.destroy_all
+		Contact.destroy_all
+		@user = User.create(phone_number: "+14033030220",
+												email: "nick@nick.ca",
+												password: "password")
+		@contact = Contact.create(phone_number: "+14039991234", name: "Han Solo")
+		@user.contacts << @contact
+		params = { id: @contact.id }
+		fake_session = { 'rack.session' => { user_id: @user.id } }
+
+		## Act
+		get "/contacts/#{@contact.id}", params, fake_session
+
+		## Assert
+		expect(last_response.body).to eq(@contact.to_json)
+
+	end
+
+end
+
+describe "get /contacts/all route" do
+
+	it "sends back a partial with all contacts' information" do
+		## Arrange
+		User.destroy_all
+		Contact.destroy_all
+		@user = User.create(phone_number: "+14033030220",
+												email: "nick@nick.ca",
+												password: "password")
+		@contact1 = Contact.create(phone_number: "+14039991234", name: "Han Solo")
+		@contact2 = Contact.create(phone_number: "+14039994321", name: "Luke Skywalker")
+		@user.contacts << @contact1
+		@user.contacts << @contact2
+		params = {}
+		fake_session = { 'rack.session' => { user_id: @user.id } }
+
+
+		## Act
+		get "/contacts/all", params, fake_session
+
+
+		# Assert
+		expect(last_response.body).to include("#{@contact1.name}")
+		expect(last_response.body).to include("#{@contact2.name}")
+
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
